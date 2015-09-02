1a843a6d36d6a0f91bd9a56d54d88a59d8e71cba
Nilkesh Surana <nilkesh.surana@tiudip.nl>
2015-09-02 17:58:15 +0530

    [#22879]:TO fetch student information from json file and add functionality like add,update,deletestudent information from table

---
+++ js/data-json-jquery.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+	$.getJSON("data/data.json",function(data){
+		$.each(data, function(i,field){
+			$("#tmain").append('<tr><td>'+field.FirstName+'</td><td>'+field.LastName+
+				'</td><td>'+field.RollNo+'</td><td>'+field.Subject+
+				'</td><td><button class="edit" >' + 'edit' +'</button></td><td><button class="rem">remove</td></tr>');
+		});
+
+	});
+
+	$("#add").on('click', function(){
+		$("#fo").css("display", "block");
+	});
+	
+	
+
+	$("#submit").on('click', function(){
+		$("#tmain").append('<tr><td>'+ $('#FirstName').val() +'</td><td>'+ $("#LastName").val() +'</td><td>'+ $("#RollNo").val() +'</td><td>'+ $("#Subject").val() +
+				'</td><td><button class="edit">edit</button></td><td><button class="rem">remove</td></tr>');
+	});
+
+	$(".rem").on('click', function(){
+		console.log("duhd");
+		$(this).parent().parent().css('color', 'green');
+	});
+
+});
+
+
